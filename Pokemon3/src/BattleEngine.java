0b05937f2dbd80900dfc0a3ff89b436a1b427425
briany2 <32102038+briany2@users.noreply.github.com>
2017-12-01 21:31:55 -0600

    Test2

---
+++ Pokemon3/src/BattleEngine.java
@@ -0,0 +1,6 @@
+
+public class BattleEngine {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+}
--- Pokemon3/src/Pokemon.java
+++ Pokemon3/src/Pokemon.java
@@ -1,4 +1,6 @@
 
 public class Pokemon {
-
+	public Pokemon() {
+		// Empty Constructor
+	}
 }
