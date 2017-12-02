93f3c801a4071d4f0c494ca7545db39720a58a8f
briany2 <32102038+briany2@users.noreply.github.com>
2017-12-01 22:32:27 -0600

    Delete BattleEngine.java

--- Pokemon3/src/BattleEngine.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class BattleEngine {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Pokemon Battle Simulator!");
-		Scanner kb;
-	}
-}
